ab89efa0c5aa69e73369191c89737b6ec208a7f3
color.ui <abhimanyuselvan@gmail.com>
2016-06-14 21:11:20 +0200

    Basic Framework

---
+++ camReader/ImageFactory.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef IMAGEFACTORY_H
+
+namespace imageFactory
+{
+	class ImageFactory
+	{
+	public:
+		ImageFactory() {}
+		~ImageFactory() {}
+	};
+}
+#endif // !IMAGEFACTORY_H
---
+++ camReader/main.cpp
@@ -0,0 +1,14 @@
+/// Entry point of the system
+
+// OpenCV headers
+#include <opencv\cv.h>   
+#include <opencv2/opencv.hpp>
+#include <opencv\highgui.h>
+
+// local headers 
+#include "ImageFactory.h"  
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}

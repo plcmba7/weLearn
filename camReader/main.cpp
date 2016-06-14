35c66e5870683a021d0fa071aa7a9e00961e7287
A2ARevolution <pinkman.org@gmail.com>
2016-06-15 00:05:55 +0200

    Accessing webcam | solved linker errors

--- camReader/main.cpp
+++ camReader/main.cpp
@@ -1,14 +1,43 @@
-/// Entry point of the system
+/*  For description look into the help() function. */
 
-// OpenCV headers
-#include <opencv\cv.h>   
-#include <opencv2/opencv.hpp>
-#include <opencv\highgui.h>
+#include "opencv\cv.h"  
+#include "opencv2/core/core.hpp"
+#include "opencv2/opencv.hpp"
+#include "opencv\highgui.h"
 
-// local headers 
+// other headers 
 #include "ImageFactory.h"  
+#include <iostream>
 
-int main(int argc, char *argv[])
+using namespace std;
+using namespace cv;
+
+static void help()
 {
+	cout
+		<< "\n--------------------------------------------------------------------------" << endl
+		<< "This program shows how to extract the video from the webcamera" << endl
+		<< "--------------------------------------------------------------------------" << endl
+		<< endl;
+}
+
+int main(int, char**)
+{
+	VideoCapture cap(0); // open the default camera
+	if (!cap.isOpened())  // check if we succeeded
+		return -1;
+
+	Mat edges;
+	namedWindow("edges", 1);
+	for (;;)
+	{
+		Mat frame;
+		cap >> frame; // get a new frame from camera
+		cvtColor(frame, edges, COLOR_BGR2GRAY);
+		cv::GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
+		Canny(edges, edges, 0, 30, 3);
+		imshow("edges", edges);
+		if (waitKey(30) >= 0) break;
+	}
 	return 0;
 }

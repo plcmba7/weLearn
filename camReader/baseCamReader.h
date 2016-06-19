57482c050a604334b430abf28452e223b06486f2
A2ARevolution <pinkman.org@gmail.com>
2016-06-19 11:51:58 +0200

    re-organising

---
+++ camReader/baseCamReader.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef BASECAMREADER_H
+
+/// Abstract base class that acts the interface.
+/// Note: If you are deriving from this class, implement the virtual methods ( at least a dummy implementation
+/// is necessary) 
+
+class baseCamReader
+{
+public:
+	baseCamReader() {}
+	~baseCamReader() {}
+
+	virtual void Init() = 0;
+	virtual void Input() = 0;
+	virtual void Tick() = 0;
+	virtual void Output() = 0;
+	virtual void Exit() = 0;
+};
+#endif // !BASECAMREADER_H
--- camReader/main.cpp
+++ camReader/main.cpp
@@ -34,8 +34,8 @@ int main(int, char**)
 		Mat frame;
 		cap >> frame; // get a new frame from camera
 		cvtColor(frame, edges, COLOR_BGR2GRAY);
-		cv::GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
-		Canny(edges, edges, 0, 30, 3);
+		//cv::GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
+		//Canny(edges, edges, 0, 30, 3);
 		imshow("edges", edges);
 		if (waitKey(30) >= 0) break;
 	}
